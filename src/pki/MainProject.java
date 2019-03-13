93eae31305c3e974435e36cf470f23153727fd84
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-13 10:07:31 +0100

    add "policz" multiplying method

--- src/pki/MainProject.java
+++ src/pki/MainProject.java
@@ -2,6 +2,10 @@ package pki;
 
 public class MainProject {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world! " + policz(2, 3));
+    }
+
+    public static int policz(int a, int b) {
+        return a * b;
     }
 }
